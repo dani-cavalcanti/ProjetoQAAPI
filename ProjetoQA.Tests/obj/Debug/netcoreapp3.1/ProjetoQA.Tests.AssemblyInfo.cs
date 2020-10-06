a64190251ede1f28038f95d303906602995d104c
Nan Cavalcanti <danicacoelho@gmail.com>
2020-10-06 11:46:15 -0300

    Criando Testes Unitários

--- ProjetoQA.Tests/obj/Debug/netcoreapp3.1/ProjetoQA.Tests.AssemblyInfo.cs
+++ ProjetoQA.Tests/obj/Debug/netcoreapp3.1/ProjetoQA.Tests.AssemblyInfo.cs
@@ -11,8 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Mvc.Versioning")]
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoQA.Tests")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProjetoQA.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProjetoQA.Tests")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
